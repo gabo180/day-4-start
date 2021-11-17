532411d4679158c22c1ad7c0ef66c1291e23efe5
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-17 14:30:46 +0000

    Comment in line 6

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import random
 random_integer = random.randint(1,10)
 print(random_integer)
 
-random_float = (random.random() * 10) / 2 #Es lo mismo que * 5 Tarita!
+random_float = (random.random() * 10) / 2 #Same as *5!
 print(random_float)
